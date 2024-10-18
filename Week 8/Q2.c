aa29060396cc90cc61412a3bd3014609fb033e3b
Skshee <suvanshee@gmail.com>
2024-10-19 00:31:03 +0530

    Update Q2.c

--- Week 8/Q2.c
+++ Week 8/Q2.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Define the Node structure
 struct Node {
     int data;
     struct Node* prev;
     struct Node* next;
 };
 
-
+// Create a new node with the given data
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
@@ -16,69 +17,24 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
-
-void concatenate(struct Node** X1, struct Node* X2) {
-    if (*X1 == NULL) {
-        *X1 = X2;
-        return;
+// Concatenate two doubly linked lists and return the head of the concatenated list
+struct Node* concatenate(struct Node* X1, struct Node* X2) {
+    if (X1 == NULL) {
+        return X2; // If the first list is empty, return the second list
     }
 
-    struct Node* temp = *X1;
+    struct Node* temp = X1;
     while (temp->next != NULL) {
-        temp = temp->next;
+        temp = temp->next; // Traverse to the end of the first list
     }
 
-    temp->next = X2;
+    temp->next = X2; // Link the last node of the first list to the head of the second list
     if (X2 != NULL) {
-        X2->prev = temp;
-    }
-}
-
-void printList(struct Node* head) {
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-
-struct Node* createList(int n) {
-    struct Node* head = NULL;
-    struct Node* temp = NULL;
-    struct Node* newNode = NULL;
-    int data;
-
-    for (int i = 0; i < n; i++) {
-        printf("Enter data for node %d: ", i + 1);
-        scanf("%d", &data);
-        newNode = createNode(data);
-
-        if (head == NULL) {
-            head = newNode;
-            temp = head;
-        } else {
-            temp->next = newNode;
-            newNode->prev = temp;
-            temp = temp->next;
-        }
+        X2->prev = temp; // Set the previous pointer of the second list's head
     }
 
-    return head;
+    return X1; // Return the head of the concatenated list
 }
 
-int main() {
-    int n1, n2;
-    printf("Enter the number of nodes for the first list: ");
-    scanf("%d", &n1);
-    struct Node* X1 = createList(n1);
-    printf("Enter the number of nodes for the second list: ");
-    scanf("%d", &n2);
-    struct Node* X2 = createList(n2);
-    concatenate(&X1, X2);
-    printf("Concatenated list: ");
-    printList(X1);
-
-    return 0;
-}
+// Print the elements of a doubly linked list
+void printList(struct Node
